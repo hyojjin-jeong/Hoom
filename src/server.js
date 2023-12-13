4e2360b96558c8a09f97fa3eafd29599d9bf333e
정효진 <jeonghyojin@keunhyojing.local>
2023-12-14 01:54:13 +0900

    누군가 방에 들어왔다는 공지 띄우기

--- src/server.js
+++ src/server.js
@@ -15,12 +15,10 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-    socket.onAny((event) => {
-        console.log(`Socket Event: ${event}`);
-    });
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
+        socket.to(roomName).emit("welcome");
     });
 });
 
